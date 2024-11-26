97414bb7b27d9ca6d6d806fac9275a24ee13c680
gustavovvbs <gustavovvbs@al.insper.edu.br>
2024-11-26 11:42:26 -0300

    email working with basic functionality

--- app/services/email.py
+++ app/services/email.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart 
 from email.mime.text import MIMEText 
 
-# Carregar variáveis de ambiente
 load_dotenv()
 
 class EmailService:
@@ -31,16 +30,34 @@ class EmailService:
                 server.starttls()
                 server.login(self.sender_email, self.sender_password)
 
+                message = MIMEMultipart("alternative")
+                message["Subject"] = "Lista de Estudos Clínicos Relacionados"
+                message["From"] = self.sender_email
+                message["To"] = email
+                
                 study_list_items = ""
                 for i, study in enumerate(studies):
+                    locations = study.get("Location", [])
+                    location_str = "No location available"
+                    if locations and isinstance(locations, list) and len(locations) > 0:
+                        first_loc = locations[0]
+                        location_str = f"{first_loc.get('City', 'N/A')} - {first_loc.get('Country', 'N/A')}"
+
+                    contacts = study.get("Contacts", [])
+                    contacts_str = "No contacts available"
+                    if contacts and isinstance(contacts, list) and len(contacts) > 0:
+                        for contact in contacts:
+                            contacts_str = f"{contact.get('name', 'N/A')} - {contact.get('email', 'N/A')}"
+                            break
+
                     study_list_items += f"""
                     <h2>{i+1}. {study.get("Title", "Sem título")}</h2>
-                    <p><strong>Condições:</strong>: {",".join(study.get("Conditions", "Sem condições informadas"))}</p>
+                    <p><strong>Condições:</strong> {", ".join(study.get("Conditions", ["Sem condições informadas"]))}</p>
                     <p><strong>Instituição:</strong>: {study.get("Organization", "Sem instituição informada")}</p>
                     <p><strong>Descrição:</strong>: {study.get("Description", "Sem descrição informada")}</p>
                     <p><strong>Intervenções</strong>: {",".join(study.get("InterventionNames", []))}</p>
-                    <p><strong>Localização</strong>: {",".join(study.get("Location", []))}</p>
-                    <p><strong>Contatos</strong>: {",".join(study.get("Contacts", []))}</p>
+                    <p><strong>Localização</strong>: {location_str}</p>
+                    <p><strong>Contatos</strong>: {contacts_str} </p>
                     <p><strong>Restrições</strong>: {study.get("Restrictions", "Sem restrições informadas")}</p>
                     <hr>
                     """
@@ -58,8 +75,11 @@ class EmailService:
                     </body>
                 </html>
                 """
-                server.sendmail(self.sender_email, email, body.encode("utf-8"))
 
+                part = MIMEText(body, "html")
+                message.attach(part)
+                
+                server.send_message(message)
                 return "Email sent successfully"
 
         except SMTPException as e:
